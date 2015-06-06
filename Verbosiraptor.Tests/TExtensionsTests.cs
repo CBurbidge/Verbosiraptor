83f40659213e6e9d3d01af3a6ecdf28b6a705960
Chester Burbidge <chesterburbidge@gmail.com>
2015-06-06 12:48:46 +0100

    I like the way python uses the in operator to test for elements in arrays. This is kind of that.

---
+++ Verbosiraptor.Tests/TExtensionsTests.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+
+namespace Verbosiraptor.Tests
+{
+	[TestFixture]
+	public class TExtensionsTests
+	{
+		public class Usages
+		{
+			public void IfThingInThings()
+			{
+				var things = new[] {"one", "two"};
+				var thing = "";
+
+				if (thing.IsIn(things))
+				{
+					// Do stuff...
+				}
+			}
+		}
+
+		[Test]
+		public void IfThingInThings_WorksWithStrings()
+		{
+			var things = new[] {"one", "two"};
+			var thing = "one";
+			
+			Assert.That(thing.IsIn(things));
+		}
+
+		[Test]
+		public void IfThingInThings_WorksWithThings()
+		{
+			var thing1 = new Thing("one", "two");
+			var thing2 = new Thing("three", "four");
+			var things = new[] {
+				thing1,
+				thing2
+			};
+			var thing = thing1;
+			
+			Assert.That(thing.IsIn(things));
+		}
+	}
+
+	public class Thing
+	{
+		private string _one;
+		private string _two;
+
+		public Thing(string one, string two)
+		{
+			_one = one;
+			_two = two;
+		}
+	}
+}
---
+++ Verbosiraptor/TExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Verbosiraptor
+{
+	public static class TExtensions
+	{
+		public static bool IsIn<T>(this T thing, IEnumerable<T> things)
+		{
+			return things.Contains(thing);
+		}
+	}
+}
